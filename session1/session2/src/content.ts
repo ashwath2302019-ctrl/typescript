74bfd62b053f00462fe5ea4fece3effaec7039e9
ASHWATH S K <ashwath.2302019@srec.ac.in>
2026-07-01 17:01:16 +0530

    Delete session1/session2 directory

--- session1/session2/src/content.ts
+++
@@ -1,5 +0,0 @@
-import { slugify, truncate } from "./helpers.js";
-
-// Use both functions here with typed inputs and observe what TypeScript knows about them
-const slug = slugify("Hello World");
-const short = truncate("This is a long text", 10);
